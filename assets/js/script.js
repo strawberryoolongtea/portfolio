a7ce66f36747aad070cbbb7e9b638ae4df9cd18e
strawberryoolongtea <strawberrycream@kakao.com>
2021-05-08 15:01:48 +0900

    modify

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,15 @@
-'use strict';
+// window.addEventListener('scroll', () => {
+//   let pageYOffset = window.pageYOffset;
+//   let backspace = document.querySelector('.page__projects--backspace');
+//   let getInTouch = document.querySelector('#get-in-touch');
+//   let scroll = document.querySelector('.scroll');
+
+//   scroll.innerHTML = pageYOffset;
+
+
+//   if(backspace.offsetTop < pageYOffset){
+//     backspace.style.position = 'fixed';
+//   } else {
+//     backspace.style.position = 'relative';
+//   }
+// });
